818483945716dffbc0419e13f8eeffd6064a5094
Roshan-Barkane <roshanbaekhane987@gmail.com>
2023-11-27 19:15:14 +0530

    write the approch solve gcf problem

--- array easy/GCForHFC.cpp
+++ array easy/GCForHFC.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 int main()
 {
+
+    /* Greatest Comman Factor
+    approch :
+    step 1:- find the min of two no of a and b.
+    step 2:- run loop 1 to min and inside the loop follow step 3.
+    step 3:- check a and b are divide i and remainder is 0 the store the i from anothe variable.
+    */
     int a = 28, b = 42;
     int min = (a < b) ? a : b;
     int gcf;
